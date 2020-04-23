13fbf1ae8e3be93d23e6d2acdd59db9519a323c1
Kaan Ozyurekli <ozyureklikaan@gmmail.com>
2020-04-24 00:34:35 +0300

    First build.

---
+++ dist/precache-manifest.7449947a3735effb742b2ec05ccd847a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b30d5a27d82953ed83",
+    "url": "/css/app.bfc0d746.css"
+  },
+  {
+    "revision": "4836ea25151fcffde1a8",
+    "url": "/css/chunk-2ca63e27.3cc59d4e.css"
+  },
+  {
+    "revision": "cc3c6cc63e8d234d1d3c",
+    "url": "/css/chunk-b2872016.d3b6c689.css"
+  },
+  {
+    "revision": "fab3a334aa3e2bddba53",
+    "url": "/css/chunk-dfb837a4.bcd3ee58.css"
+  },
+  {
+    "revision": "25af970f2a45bdeb078b",
+    "url": "/css/chunk-vendors.553ee0b2.css"
+  },
+  {
+    "revision": "e8b2072014be7ff3c66c16d845761b27",
+    "url": "/img/forest.e8b20720.jpg"
+  },
+  {
+    "revision": "b9e191ca6b4825c53194a839d500c584",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7b30d5a27d82953ed83",
+    "url": "/js/app.e267023f.js"
+  },
+  {
+    "revision": "4836ea25151fcffde1a8",
+    "url": "/js/chunk-2ca63e27.221fb51b.js"
+  },
+  {
+    "revision": "cc3c6cc63e8d234d1d3c",
+    "url": "/js/chunk-b2872016.7b155092.js"
+  },
+  {
+    "revision": "fab3a334aa3e2bddba53",
+    "url": "/js/chunk-dfb837a4.fe3fa64c.js"
+  },
+  {
+    "revision": "25af970f2a45bdeb078b",
+    "url": "/js/chunk-vendors.051b2331.js"
+  },
+  {
+    "revision": "1cc2210da8e9a4d6000e0ce72be6fa89",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
